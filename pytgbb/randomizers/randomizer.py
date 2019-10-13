bc23bd2555d1ed43bb84f0261d398d049c730640
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2019-10-13 22:45:15 +0200

    add(randomizers): add ABC for randomizers

---
+++ pytgbb/randomizers/randomizer.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from random import Random
+
+class Randomizer(ABC):
+
+    def __init__(self):
+        self.random = Random()
+        self.random.seed()
+
+    def seed(self, *args, **kwargs):
+        self.random.seed(*args, **kwargs)
+
+    @abstractmethod
+    def draw(self):
+        pass

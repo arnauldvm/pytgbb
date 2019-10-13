2f8b131fd94d7fe09f38368c2286e7f703d45c51
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2019-10-14 00:14:03 +0200

    fix(setup): clean egg-info dir when building

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
+from os.path import isdir
 from setuptools import setup, find_namespace_packages
+from shutil import rmtree
 
+egg_info_dir = 'pytgbb.egg-info'
+if isdir(egg_info_dir):
+    rmtree(egg_info_dir)
 
 setup(packages = find_namespace_packages(include=['pytgbb.*']))

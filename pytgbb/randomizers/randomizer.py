d11660d5b0821aaa877a3b79f7fe92e69fdb5a05
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2019-10-16 00:56:15 +0200

    doc(randomizers): add code documentation

--- pytgbb/randomizers/randomizer.py
+++ pytgbb/randomizers/randomizer.py
@@ -6,22 +6,44 @@ T = TypeVar('T')
 
 
 class Randomizer(ABC, Generic[T]):
+    """
+    A Randomizer is a component that generate results randomly.
+
+    The results can be any type such as: an int, a color, a Card...
+    """
 
     def __init__(self):
+        """The constructor of th abstract class akes care of initializing the random generator."""
         self.random = Random()
         self.random.seed()
 
     def seed(self, *args, **kwargs):
+        """Resets the random generator. Takes the same arguments as the `Random.seed(...)` method"""
         self.random.seed(*args, **kwargs)
 
     @abstractmethod
     def draw(self) -> T:
+        """
+        Generates **one** random result.
+
+        This must be implemented by the subclasses.
+        """
         pass
 
     @abstractmethod
     def draws(self, count: int) -> List[T]:
+        """
+        Generates *count* random results.
+
+        This must be implemented by the subclasses.
+        """
         pass
 
     @abstractmethod
     def __len__(self) -> int:
+        """
+        Returns the count of possible result values.
+
+        This must be implemented by the subclasses.
+        """
         pass
--- pytgbb/randomizers/stateless.py
+++ pytgbb/randomizers/stateless.py
@@ -4,8 +4,17 @@ from .randomizer import Randomizer, T
 
 
 class StatelessRandomizer(Randomizer, Generic[T]):
+    """
+    A Randomizer that has no memory.
+
+    The likelihood of a result value at a given time is independent from the result values that were drawn in the past.
+
+    Examples: a die, a fistful of dice, a spinner/Wheel
+    """
 
     def __init__(self, values: Union[int, Sequence[T]]):
+        """Initializes a StatelessRandomizer. Accepts either a sequence of values, or an int.
+        In the latter case, the possible value are 0 ... n-1."""
         self.values: Union[Sequence[int], Sequence[T]]
         if isinstance(values, int):
             self.values = range(values)
@@ -16,26 +25,47 @@ class StatelessRandomizer(Randomizer, Generic[T]):
         super(StatelessRandomizer, self).__init__()
 
     def draw(self) -> T:
+        """
+        Generates **one** random result.
+
+        This should not be overriden by the subclasses.
+        """
         return self.random.choice(self.values)
 
     def draws(self, count: int) -> List[T]:
+        """
+        Generates *count* random results.
+
+        This should not be overriden by the subclasses.
+        """
         return [self.draw() for _ in range(count)]
 
     def __len__(self) -> int:
+        """
+        Returns the count of possible result values.
+
+        This should not be overriden by the subclasses.
+        """
         return len(self.values)
 
 
 class Die(StatelessRandomizer, Generic[T]):
+    """This class emulates rolling a die."""
 
     def __init__(self, sides: Union[int, Sequence]):
+        """Initializes a Die. Accepts either a sequence of values, or an int.
+        In the latter case, the possible value are 1 ... n."""
         if isinstance(sides, int):
             sides = range(1, sides+1)
         super(Die, self).__init__(sides)
 
 
 class Spinner(StatelessRandomizer):
+    """This class emulates spinning the arrow of a wheel. Alias: Wheel"""
 
     def __init__(self, sectors: Union[int, Sequence]):
+        """Initializes a Spinner. Accepts either a sequence of values, or an int.
+        In the latter case, the possible value are 1 ... n."""
         if isinstance(sectors, int):
             sectors = range(1, sectors+1)
         super(Spinner, self).__init__(sectors)
